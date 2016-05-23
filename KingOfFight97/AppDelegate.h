10735c0b414db9aebc0de3d14cd2d3f86d21e375
molake <jian.a.lu@accenture.com>
2016-05-23 14:17:35 +0800

    Initial Commit

---
+++ KingOfFight97/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KingOfFight97
+//
+//  Created by molake on 16/5/23.
+//  Copyright © 2016年 molake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KingOfFight97/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KingOfFight97
+//
+//  Created by molake on 16/5/23.
+//  Copyright © 2016年 molake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
